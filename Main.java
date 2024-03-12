5216594656e9dc55a73cd4245dfaccf4a47fe0de
Michael <90722679+MichaelJP2002@users.noreply.github.com>
2024-03-12 12:56:48 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Today is March 12th");
         System.out.println("Testing testing");
         System.out.println("IDK");
+        System.out.println("Best branch!!!");
     }
 }
