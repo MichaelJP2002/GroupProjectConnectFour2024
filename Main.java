ebe6462fb0389db20e5672f6992edc7e63cc858b
Michael <90722679+MichaelJP2002@users.noreply.github.com>
2024-03-19 13:31:32 -0400

    Progress!

--- Main.java
+++ Main.java
@@ -1,9 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
-        System.out.println("Today is March 12th");
-        System.out.println("Testing testing");
-        System.out.println("IDK");
-        System.out.println("Best branch!!!");
+
+
+        JFrame frame = new JFrame("CHESS GAME");
+        frame.getContentPane().setBackground(Color.black);
+        frame.getRootPane().setBorder(BorderFactory.createMatteBorder(4,4,4,4, Color.ORANGE));
+        // Size our board
+        frame.setMinimumSize(new Dimension(900,900));
+        // Grid
+        frame.setLayout(new GridBagLayout());
+        //
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Make board visible
+        //frame.setVisible(true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
