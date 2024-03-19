b22d9f7bfaf9baf127261e5ef699ec64817e77fd
peterhouse7 <housepeter405@gmail.com>
2024-03-19 13:30:56 -0400

    lijioi

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
-        System.out.println("this branch betta");
-
+        JFrame frame = new JFrame("CONNECT 4");
+        JPanel panel = (JPanel) frame.getContentPane();
+        panel.setLayout(new GridLayout(6,7));
+        frame.setContentPane(panel);
+        frame.setSize(
+                700, 600);
+        frame.setVisible(
+                true);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel.setBackground(Color.red);
+        
     }
 }
