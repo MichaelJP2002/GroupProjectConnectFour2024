3d9297bafe1155f6c5ccf155d9a783ffb66d6d38
Michael <90722679+MichaelJP2002@users.noreply.github.com>
2024-03-25 13:46:02 -0400

    Progress!

--- ConnectFour.java
+++ ConnectFour.java
@@ -5,45 +5,39 @@ import java.awt.*;
 
 public class ConnectFour {
 
-    //55
+
     public static final int BOARD_SIZE = 355, TILE_SIZE = BOARD_SIZE / 8;
     public static int numberOfMoves = 0;
     public static ConnectGUI board;
-  // public static Player player = new Player();
-   // public static Piece [][] position = new Piece[8][8];
 
 
     public static void main (String [] args) {
-//Setting White and Black Pieces Positions.
-        // Draw pieces in appropriate starting positions.
-        // Black Rooks
 
 
-        //Add Pawn Black and White
+
+
         for(int x = 0; x <= 7; x++) {
 
         }
-        // Start making board
+
         JFrame frame = new JFrame("CONNECT FOUR");
         frame.getContentPane().setBackground(Color.black);
         frame.getRootPane().setBorder(BorderFactory.createMatteBorder(4,4,4,4, Color.ORANGE));
-        // Size our board
+
         frame.setMinimumSize(new Dimension(900,900));
-        // Grid
+
         frame.setLayout(new GridBagLayout());
-        //
+
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // Make board visible
+
 
         board = new ConnectGUI();
 
-        // board = new boardComponent();
-       // BoardMouseListener boardMouse  = new BoardMouseListener();
-      //  board.addMouseListener(boardMouse);
+
         frame.add(board);
         frame.pack();
-        // Make the board visible
+
         frame.setVisible(true);
 
     }
--- ConnectGUI.java
+++ ConnectGUI.java
@@ -1,7 +1,12 @@
+import java.awt.Graphics;
 import javax.swing.*;
 import java.awt.*;
 
 public class ConnectGUI extends JComponent {
+
+
+
+
 
     int column = 8;
     int row = 8;
@@ -10,25 +15,34 @@ public class ConnectGUI extends JComponent {
         this.setPreferredSize(new Dimension(column*ConnectFour.TILE_SIZE, row*ConnectFour.TILE_SIZE));
     }
 
+
+
+
     public void paintComponent(Graphics graphics) {
         Graphics2D graphics2D = (Graphics2D) graphics;
-        // Nested for loop to track x and y position.
+
+
+
+
+
         for(int y = 0; y <= 7; y++) {
             for (int x = 0; x <= 7; x++) {
-                // If both x AND y are even, OR x AND y are odd, paint the lighter rectangle.
+
                 if((x % 2 == 0 && y % 2 == 0) || (x % 2 == 1 && y % 2 == 1)) {
-                    graphics2D.setColor(new Color(0, 0, 255));
+                    graphics2D.setColor(new Color(0, 125, 200));
                 }
-                // Otherwise, paint the darker rectangle.
+
                 else {
-                    graphics2D.setColor(new Color(0, 0, 255));
+                    graphics2D.setColor(new Color(0, 125, 200));
                 }
                 graphics2D.fillRect(x * ConnectFour.TILE_SIZE, y * ConnectFour.TILE_SIZE, ConnectFour.TILE_SIZE, ConnectFour.TILE_SIZE);
-                // If the position within the array is NOT null, run method drawPiece within the Piece class.
-               // if(Chess.position[x][y] != null) {
-                   // Chess.position[x][y].drawPiece(x * 44, y * 44, graphics);
-                    //System.out.println(Chess.position[x][y].PieceType);
+
+
+
                 }
+
+
+
             }
         }
     }
