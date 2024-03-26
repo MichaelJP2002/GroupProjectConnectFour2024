6914eb0e1379afa636a3165d7d648fef5a764634
Michael <90722679+MichaelJP2002@users.noreply.github.com>
2024-03-26 13:08:24 -0400

    Progress!

--- ConnectGUI.java
+++ ConnectGUI.java
@@ -25,24 +25,42 @@ public class ConnectGUI extends JComponent {
 
 
 
-        for(int y = 0; y <= 7; y++) {
-            for (int x = 0; x <= 7; x++) {
-
-                if((x % 2 == 0 && y % 2 == 0) || (x % 2 == 1 && y % 2 == 1)) {
-                    graphics2D.setColor(new Color(0, 125, 200));
-                }
-
-                else {
-                    graphics2D.setColor(new Color(0, 125, 200));
-                }
-                graphics2D.fillRect(x * ConnectFour.TILE_SIZE, y * ConnectFour.TILE_SIZE, ConnectFour.TILE_SIZE, ConnectFour.TILE_SIZE);
 
 
 
-                }
+        graphics.drawRect(0,0,5000,5000);
+        graphics.setColor(new Color(0,125,200));
+        graphics.fillRect(0,0,5000,5000);
+
+
+
+
+        for(int i = 0; i < 8; i++) {
+            graphics.drawOval(10, 10, 30, 30);
+            graphics.setColor(Color.black);
+            graphics.fillOval(10, 10, 30, 30);
+        }
+
+
+
+       // for(int y = 0; y <= 7; y++) {
+          //  for (int x = 0; x <= 7; x++) {
+
+              // if((x % 2 == 0 && y % 2 == 0) || (x % 2 == 1 && y % 2 == 1)) {
+              //      graphics2D.setColor(new Color(0, 0, 0));
+             //   }
+
+             //   else {
+             //       graphics2D.setColor(new Color(0, 0, 0));
+             //   }
+              //  graphics2D.fillRect(x * ConnectFour.TILE_SIZE, y * ConnectFour.TILE_SIZE, ConnectFour.TILE_SIZE, ConnectFour.TILE_SIZE);
+
+
+
+               // }
 
 
 
             }
         }
-    }
+    //}
